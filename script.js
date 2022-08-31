5b2bfc6e233e131a0c4dceb2397f9b2265f258b1
Andrew Grenon <andrew.grenon@gmail.com>
2022-08-31 12:20:06 +1200

    Update lesson so far

---
+++ script.js
@@ -0,0 +1,66 @@
+console.log("Andrew Grenon")
+///
+
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+console.log("Andrew Grenon")
+/// 
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+console.log(9);
+console.log(10);
+
+// for loops 
+
+//initialise variable
+let aVar = 1 
+
+// for loop ---> (initialisation; condition; action) {output}
+for (aVar = 1; aVar <= 5; aVar++) {
+    console.log(aVar);
+}
+
+// from our example earlier a better solution using for loops would be:
+
+for (i = 1; i <= 10; i++) {
+    console.log(i)
+}
+
+// counting down from 10-1
+
+for (i = 10; i > 0; i--) {
+  console.log(i);
+}
+
+//rendering first 10 letters of a string
+
+let word = "hippopotamus"
+
+for (i = 0; i <=9; i++) {
+   console.log( word[i])
+}
+
